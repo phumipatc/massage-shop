7c2dce573ff7a6ab21732f2b8b760cdbe5064d4c
Phumipat Chaiprasertsud <phumipat.c@outlook.com>
2023-11-03 01:25:26 +0700

    Add "My Reservations" page

---
+++ src/components/ReservationCard.tsx
@@ -0,0 +1,23 @@
+import Reservation from "@/types/reservation";
+import Image from "next/image";
+
+export default function ReservationCard({key, reservation}: {key: number, reservation: Reservation}) {
+	return (
+		// background color: rgba(240, 159, 156, 1)
+		<div className='w-2/3 my-5 mx-auto md:w-5/6 md:h-64 bg-[#F09F9C] rounded-md shadow-lg md:flex overflow-hidden border-1 border-gray-600'>
+			<div className='w-full md:w-2/3 h-1/2 md:h-full'>
+                <div className='flex justify-between h-1/4 pt-16 pl-6 lg:pl-24 items-center'>
+                    <h1 className='text-2xl lg:text-3xl font-bold hover:text-clip'>{reservation.name}</h1>                    
+                </div>
+                <div className='flex flex-col gap-1 pt-5 pl-10 lg:pl-32 pb-10 mt-2 md:mt-5 mb-2 md:mb-0'>
+					<p className='text-base lg:text-xl font-semibold'>Date: {reservation.date}</p>
+					<p className='text-base lg:text-xl font-semibold'>Duration: {reservation.duration}</p>
+					<p className='text-base lg:text-xl font-semibold'>Telephone number: {reservation.phone}</p>
+                </div>
+            </div>
+			<div className='w-full md:w-1/3 h-1/2 md:h-full md:shrink-0'>
+					<Image width={1000} height={1000} src={reservation.picture} alt={reservation.name} className='md:h-full w-full object-cover'/>
+			</div>
+		</div>
+	);
+}
--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,15 +1,19 @@
 import Image from 'next/image'
+import Link from 'next/link'
 export default function TopMenu(){
     return (
         <div className="w-full h-20 flex fixed top-0 z-50">
             <div className='w-1/2 md:w-1/4 h-full bg-[#C76B98] flex items-center px-5'>
-                <Image src='/img/logo-no-background.png' alt='logo' width={1500} height={647} className='h-12 md:h-14 w-fit'/>
+                <Link href='/'>
+                    <Image src='/img/logo-no-background.png' alt='logo' width={1500} height={647} className='h-12 md:h-14 w-fit'/>
+                </Link>
             </div>
             <span className='absolute h-10 w-10 rounded-[50%] left-1/2 md:left-1/4 ml-[-1.25rem] bg-[#C76B98]' />
             <span className='absolute h-10 w-10 rounded-[50%] left-1/2 md:left-1/4 ml-[-1.25rem] top-10 bg-[#632B6C]' />
             <div className='w-1/2 md:w-3/4 h-full bg-[#632B6C] flex flex-row-reverse px-5 items-center '>
                 {/* <button>HI</button> */}
-                <button className="h-11 bg-white w-fit p-2">My reservation</button>
+                {/* if click link to "/reservations" */}
+                <Link className="h-11 bg-white w-fit p-2" href="/reservations">My reservations</Link>
             </div>
         </div>
     )
