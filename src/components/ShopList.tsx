e964348fed21ad363fa585579a227a5feacefe3a
Phumipat Chaiprasertsud <phumipat.c@outlook.com>
2023-11-22 00:16:08 +0700

    Able to search shop with name and price

--- src/components/ShopList.tsx
+++ src/components/ShopList.tsx
@@ -4,34 +4,63 @@ import { Suspense, useEffect, useState } from "react"
 import { LinearProgress } from "@mui/material"
 import ShopModal from "./ShopListContainer"
 import Shops from "@/types/Shops"
+import { Radio, RadioGroup } from "@nextui-org/react"
 
 export default function ShopList({profile, shops}: {profile: Object, shops: Shops}) {
 
 	// store the shop name input
 	const [searchShopName, setSearchShopName] = useState('')
 	const [filteredShop, setFilteredShop] = useState(shops)
+	const [searchPriceLevel, setSearchPriceLevel] = useState(0)
+	const pricelevel = [0, 1, 2, 3]
 
 	function searchHandler() {
-		if(searchShopName != ''){
-			console.log(searchShopName)
-			console.log(shops.data.filter(shop => shop.name.toLowerCase().includes(searchShopName.toLowerCase())))
+		console.log(searchShopName, searchPriceLevel)
+		if(searchShopName == '' && searchPriceLevel == 0){
+			setFilteredShop(shops)
+			return
+		}
+		if(searchShopName == ''){
 			setFilteredShop({
 				success: true,
-				count: shops.data.filter(shop => {shop.name.toLowerCase().includes(searchShopName.toLowerCase())}).length,
-				data: shops.data.filter(shop => {shop.name.toLowerCase().includes(searchShopName.toLowerCase())})
+				count: shops.data.filter((shop)=>shop.priceLevel == searchPriceLevel).length,
+				data: shops.data.filter((shop)=>shop.priceLevel == searchPriceLevel)
 			})
-		}else{
-			setFilteredShop(shops)
+			return
 		}
+		if(searchPriceLevel == 0){
+			setFilteredShop({
+				success: true,
+				count: shops.data.filter((shop)=>shop.name.toLowerCase().includes(searchShopName.toLowerCase())).length,
+				data: shops.data.filter((shop)=>shop.name.toLowerCase().includes(searchShopName.toLowerCase()))
+			})
+			return
+		}
+		setFilteredShop({
+			success: true,
+			count: shops.data.filter((shop)=>shop.name.toLowerCase().includes(searchShopName.toLowerCase()) && shop.priceLevel == searchPriceLevel).length,
+			data: shops.data.filter((shop)=>shop.name.toLowerCase().includes(searchShopName.toLowerCase()) && shop.priceLevel == searchPriceLevel)
+		})
 	}
 
 	return (
 		<div>
 			<div className='m-5 mt-8 px-5 flex'>
 				<div className='flex gap-3 w-11/12'>
-					<input className='border-2 border-gray-300 rounded-md px-2 w-7/12' value={searchShopName} type='text' placeholder='Shop Name' onChange={(e) => setSearchShopName(e.target.value)} />
+					<input className='border-2 border-gray-300 rounded-md px-2 w-8/12' value={searchShopName} type='text' placeholder='Shop Name' onChange={(e) => setSearchShopName(e.target.value)} />
 					{/* <button className='border-2 border-gray-300 rounded-md px-2'>Province</button> */}
-					<button className='border-2 border-gray-300 rounded-md px-2'>Price level</button>
+					{/* <button className='border-2 border-gray-300 rounded-md px-2'>Price level</button> */}
+					<RadioGroup
+						label="Price level"
+						value={searchPriceLevel.toString()}
+						onChange={(e)=>{setSearchPriceLevel(parseInt(e.target.value))}}
+						orientation="horizontal"
+						className="flex flex-row gap-x-5 py-4 border-2 border-gray-300 rounded-md px-2"
+						>
+						{pricelevel.map((level:number) => (
+							<Radio key={level} value={level.toString()}>{level==0?'Any':level}</Radio>
+						))}
+					</RadioGroup>
 				</div>
 				<button className='text-2xl font-semibold border-2 border-gray-300 rounded-md p-2 px-4' onClick={searchHandler}>Search</button>
 			</div>
