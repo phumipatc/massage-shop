772ed06549faa11c9436c4b745135a809d0d661f
Tawatwee Aiemarreerat <punnyoz1103@gmail.com>
2023-11-22 22:11:04 +0700

    add style to reservation card

--- src/components/ReservationCard.tsx
+++ src/components/ReservationCard.tsx
@@ -8,13 +8,13 @@ export default function ReservationCard({profile, reservation, selectReservation
 		// background color: rgba(240, 159, 156, 1)
 		<div className='w-2/3 my-5 mx-auto md:w-5/6 md:h-64 bg-[#F09F9C] rounded-md shadow-lg md:flex overflow-hidden border-1 border-gray-600 hover:drop-shadow-2xl transition-all'>
 			<div className='w-full md:w-2/3 h-1/2 md:h-full'>
-                <div className='flex h-1/4 pt-16 pl-6 lg:pl-24 items-center gap-x-5'>
-                    <h1 className='text-2xl lg:text-3xl font-bold hover:text-clip'>{reservation.shop.name}</h1>    
+                <div className='flex h-fit p-6 lg:pl-24 items-center gap-x-5 bg-[#632B6C] text-white'>
+                    <h1 className='text-xl lg:text-3xl font-bold hover:text-clip'>{reservation.shop.name}</h1>    
 					<button onClick={() => {selectReservationToEdit(reservation); onOpenModal()}}>
 						<Image src='/img/edit.png' alt='edit' width={300} height={300} className='h-10 w-10'/>
 					</button>
                 </div>
-                <div className='flex flex-col gap-1 pt-5 pl-10 lg:pl-32 pb-10 mt-2 md:mt-5 mb-2 md:mb-0'>
+                <div className='flex flex-col gap-1 pt-2 pl-10 lg:pl-32 pb-10 mt-2 md:mt-5 mb-2 md:mb-0'>
 					<p className='text-base lg:text-xl font-semibold'>Date: {dayjs(reservation.bookingDate).format('MM/DD/YYYY')}</p>
 					<p className='text-base lg:text-xl font-semibold'>Duration: {reservation.serviceMinute} minutes</p>
 					<p className='text-base lg:text-xl font-semibold'>Telephone number: {reservation.shop.tel}</p>
